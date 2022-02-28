c527f89d04e0830149702689537f94b876f1f059
Moldoteck <cpadureac@gmail.com>
2022-02-28 17:13:35 +0200

    Fix command msg

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -9,21 +9,14 @@ let bot_commands = [
   { command: 'subscribe_mod', description: 'became moderator of chat' },
   { command: 'unsubscribe_mod', description: 'unsubscribe from moderating' },
   { command: 'interactive', description: 'will respond to toxic messages' },
-  { command: 'setthresh', description: 'change threshold for category' },
-  { command: 'resetthresh', description: 'reset thresholds to defaults' },
-  { command: 'getthresh', description: 'get defaults' },
-  { command: 'toxicscore', description: 'get toxicity score for message' },
-  {
-    command: 'toxicscoreall',
-    description: 'get all toxicity scores for message',
-  },
+  { command: 'thresh', description: 'threshold details' },
   { command: 'language', description: 'change language' },
   { command: 'help', description: 'help message' },
   { command: 'hide_cmd', description: 'hide inline commands' },
   { command: 'show_cmd', description: 'show inline commands' },
   { command: 'add_trig', description: '(beta) add trigger word' },
   { command: 'rm_trig', description: '(beta) remove trigger word' },
-  { command: 'get_trig', description: '(beta) get trigger words' },
+  { command: 'list_trig', description: '(beta) get trigger words' },
 ]
 
 export function commandHandler(bot: Bot<Context>) {
